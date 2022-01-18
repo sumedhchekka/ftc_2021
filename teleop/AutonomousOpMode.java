7209065c81839e45a386295ee3efebc92d716f7a
schekka@twilio.com <schekka@twilio.com>
2022-01-18 13:33:16 -0800

    organized into functions, turning clockwise

--- teleop/AutonomousOpMode.java
+++ teleop/AutonomousOpMode.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode;
-
+ 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+ 
 @Autonomous
 public class AutonomousOpMode extends LinearOpMode {
-
+ 
   private DcMotor motorFrontLeft;
   private DcMotor motorBackLeft;
   private DcMotor motorFrontRight;
@@ -24,6 +24,7 @@ public class AutonomousOpMode extends LinearOpMode {
   static final double WHEEL_CIRCUMFERENCE_MM = 100 * Math.PI;
   static final double DRIVE_COUNTS_PER_MM = (HD_COUNTS_PER_REV * DRIVE_GEAR_REDUCTION) / WHEEL_CIRCUMFERENCE_MM;
   static final double DRIVE_COUNTS_PER_IN = DRIVE_COUNTS_PER_MM * 25.4;
+  static final double DRIVE_COUNTS_PER_ANGLE = 10.0; // Mesure and retune ?
     
   @Override
   public void runOpMode() {
@@ -35,68 +36,129 @@ public class AutonomousOpMode extends LinearOpMode {
     carouselservo = hardwareMap.get(Servo.class, "carouselservo");
     intakeservo = hardwareMap.get(Servo.class, "intakeserv");
     arm = hardwareMap.get(DcMotorEx.class, "arm");
-
-   // reverse left drive motor direciton
+ 
+    // reverse left drive motor direciton
     motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
     motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);    
     
+    // arm setup
+    arm.setDirection(DcMotorEx.Direction.REVERSE);
+    arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     
     waitForStart();
     if (opModeIsActive()) {
-      
-      
-      
-      // Create target positions
-      int rightfrontTarget = motorFrontRight.getCurrentPosition() + (int)(30 * DRIVE_COUNTS_PER_IN);
-      int leftfrontTarget = motorFrontLeft.getCurrentPosition() + (int)(30 * DRIVE_COUNTS_PER_IN);
-      int rightbackTarget = motorBackRight.getCurrentPosition() + (int)(30 * DRIVE_COUNTS_PER_IN);
-      int leftbackTarget = motorBackLeft.getCurrentPosition() + (int)(30 * DRIVE_COUNTS_PER_IN);
      
       //set the arm to the medium level
-      arm.setDirection(DcMotorEx.Direction.REVERSE);
-      arm.setMode(DcMotor.RunMode.RESET_ENCODERS);
       arm.setTargetPosition(90);
       arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      arm.setVelocity(200);    
+      arm.setVelocity(200);
       
-      //while(arm.isBusy()){
-        
-      //}
-      //intake //Turn clockwise
-     //intakeservo.setPosition(1);
+      moveForward(30, 0.7, false);
       
-      // set target position
+      while(arm.isBusy()) {}
+      intakeservo.setPosition(0);
+    
+      sleep(1500);
+       
+      // stop intake
+      intakeservo.setPosition(0.5);
+      
+      // reset arm
+      arm.setTargetPosition(-90);
+      arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      arm.setVelocity(100);
+      
+      turnClockwise(-90, 0.7, true);
+      
+      moveForward(30, 0.7, false);
+      
+      while (opModeIsActive()
+            && (motorBackLeft.isBusy() 
+                  || motorBackRight.isBusy()  
+                  || motorFrontLeft.isBusy() 
+                  || motorFrontRight.isBusy())) {}
+     
+      
+      //yuh// set motor power back to 0 
+       
+     }
+   }
+   
+   private void moveDrivetrain(int rightfrontChange, int leftfrontChange,
+                               int rightbackChange, int leftbackChange,
+                               double power, boolean waitForMotorIdle)
+  {
+      // Create target positions
+      int rightfrontTarget = motorFrontRight.getCurrentPosition() + rightfrontChange;
+      int leftfrontTarget = motorFrontLeft.getCurrentPosition() + leftfrontChange;
+      int rightbackTarget = motorBackRight.getCurrentPosition() + rightbackChange;
+      int leftbackTarget = motorBackLeft.getCurrentPosition() + leftbackChange;
+     
+      //switch to run to position mode
       motorBackLeft.setTargetPosition(leftbackTarget);
       motorBackRight.setTargetPosition(rightbackTarget);
       motorFrontLeft.setTargetPosition(leftfrontTarget);
       motorFrontRight.setTargetPosition(rightfrontTarget);
       
-      //switch to run to position mode
+      //run to position at the desiginated power
+      
       motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       
-      //run to position at the desiginated power
-      motorBackLeft.setPower(0.7);
-      motorBackRight.setPower(0.7);
-      motorFrontLeft.setPower(0.7);
-      motorFrontRight.setPower(0.7);
-    
-      while(arm.isBusy()) {}
-      intakeservo.setPosition(0);
+      motorBackLeft.setPower(power);
+      motorBackRight.setPower(power);
+      motorFrontLeft.setPower(power);
+      motorFrontRight.setPower(power);
       
-      // wait until both motors are no longer busy running to position
-      while (opModeIsActive() && (motorBackLeft.isBusy() || motorBackRight.isBusy())) {
-      }
-      
-      // set motor power back to 0 
-       
-      } 
-      
-      motorBackLeft.setPower(0);
-      motorBackRight.setPower(0);
-      motorFrontLeft.setPower(0);
-      motorFrontRight.setPower(0);
-     }
+      while (opModeIsActive()
+            && waitForMotorIdle
+            && (motorBackLeft.isBusy() 
+                  || motorBackRight.isBusy()  
+                  || motorFrontLeft.isBusy() 
+                  || motorFrontRight.isBusy())) {}
+   }
+   
+   private void moveForward(int distance, double power, boolean waitForMotorIdle)
+   {
+      // Calculate position changes for each motor
+      int change = (int)(distance * DRIVE_COUNTS_PER_IN);
+      int rightfrontChange = change;
+      int leftfrontChange = change;
+      int rightbackChange = change;
+      int leftbackChange = change;
+     
+      moveDrivetrain(rightfrontChange, leftfrontChange,
+                     rightbackChange, leftbackChange,
+                     power, waitForMotorIdle);
+   }
+   
+   private void turnClockwise(int angle, double power, boolean waitForMotorIdle)
+   {
+      // Calculate position changes for each motor
+      int change = (int)(angle * DRIVE_COUNTS_PER_ANGLE);
+      int rightfrontChange = - change;
+      int leftfrontChange = change;
+      int rightbackChange = - change;
+      int leftbackChange = change;
+     
+      moveDrivetrain(rightfrontChange, leftfrontChange,
+                     rightbackChange, leftbackChange,
+                     power, waitForMotorIdle);
+   }
+   
+   private void strafeLeft(int shift, double power, boolean waitForMotorIdle)
+   {
+      // Calculate position changes for each motor
+      int change = (int)(shift * DRIVE_COUNTS_PER_IN);
+      int rightfrontChange = change;
+      int leftfrontChange = - change;
+      int rightbackChange = - change;
+      int leftbackChange = change;
+     
+      moveDrivetrain(rightfrontChange, leftfrontChange,
+                     rightbackChange, leftbackChange,
+                     power, waitForMotorIdle);
+   }
  }
